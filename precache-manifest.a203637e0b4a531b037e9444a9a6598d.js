d071f88990dfb04b4eafcc520139de9258b3ddc4
Krishna Prasad <skprasadu@yahoo.com>
2019-02-02 20:52:31 -0800

    forth commit

---
+++ precache-manifest.a203637e0b4a531b037e9444a9a6598d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "cefea4c8c2f9011102c2",
+    "url": "/static/js/main.cefea4c8.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "cefea4c8c2f9011102c2",
+    "url": "/static/css/main.5b0aac49.chunk.css"
+  },
+  {
+    "revision": "b693793b192b71b1d70d910d81506821",
+    "url": "/index.html"
+  }
+];
